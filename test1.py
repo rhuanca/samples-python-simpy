036cb693e67ac1bb1338e49b4d21c2de444620fe
Renan Huanca <renan.huanca@mojix.com>
2018-02-14 21:45:22 -0400

    simulation with chart is working

---
+++ test1.py
@@ -0,0 +1,93 @@
+import simpy
+import random
+
+class Philosopher():
+    T0 = 10 # Mean thinking time
+    T1 = 10 # Mean eating time
+    DT = 1 # Time to pick another chopstick
+
+    def __init__(self, env, chopsticks, my_id, DIAG = True):
+        self.env = env
+        self.chopsticks = chopsticks
+        self.id = my_id
+        self.waiting = 0
+        self.DIAG = DIAG
+        # Register the process with the environment
+        env.process(self.run_the_party())
+
+    def get_hungry(self): # Request the resources
+        start_waiting = self.env.now
+        self.diag("request chopstick")
+        rq1 = self.chopsticks[0].request()
+        yield rq1
+
+        self.diag("obtained chopstick")
+        yield self.env.timeout(self.DT)
+
+        self.diag("request another chopstick")
+        rq2 = self.chopsticks[1].request()
+        yield rq2
+
+        self.diag("obtained another chopstick")
+        self.waiting += self.env.now - start_waiting
+        return rq1, rq2
+
+    def run_the_party(self): # Do everything
+        while True:
+            # Thinking
+            thinking_delay = random.expovariate(1 / self.T1)
+            yield self.env.timeout(thinking_delay)
+
+            # Getting hungry
+            get_hungry_p = self.env.process(self.get_hungry())
+            rq1, rq2 = yield get_hungry_p
+
+            # Eating
+            eating_delay = random.expovariate(1 / self.T1)
+            yield self.env.timeout(eating_delay)
+
+            # Done
+            self.chopsticks[0].release(rq1)
+            self.chopsticks[1].release(rq2)
+            self.diag("released the chopsticks")
+
+    def diag(self, message): # Diagnostic routine
+        if self.DIAG:
+            print("P{} {} @{} ".format(self.id, message, self.env.now))
+
+
+def simulate(n,t):
+    """
+    Simulate the system of n philosophers for up t time units
+    Return the average waiting time
+    """
+    env = simpy.Environment()
+    chopsticks = [simpy.Resource(env, capacity=1) for i in range(n)]
+    # philosophers = [
+    #     Philosopher(env, chopsticks[i], chopsticks[(i + 1) % n], i)
+    #     for i in range(n)]
+    philosophers = [
+        Philosopher(env, (chopsticks[i], chopsticks[(i + 1) % n]), i)
+        for i in range(n)]
+
+    env.run(until=t)
+    return sum(ph.waiting for ph in philosophers) / n
+
+
+# Set up the plotting system
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.style.use("ggplot")
+
+# Simulate
+N = 20
+X = range(2, N)
+Y = [simulate(n, 50000) for n in X]
+
+
+# Plot
+plt.plot(X, Y, "-o")
+plt.ylabel("waiting time")
+plt.xlabel("number of philosophers")
+plt.show()
